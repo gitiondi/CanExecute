5c67b6903a0b44333b84151628c92763b0a0ae1d
enrico.biondi <ebiondi@gmail.com>
2022-02-02 17:32:16 +0100

    Add project files.

---
+++ CanExecute/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+
+namespace CanExecute
+{
+    class MainViewModel : ObservableObject
+    {
+        private bool _isCheckBoxEnabled;
+        private string _title;
+        public AsyncRelayCommand BtnCmd { get; }
+        public ICommand CheckBoxCmd { get; }
+
+        public bool IsCheckBoxEnabled
+        {
+            get => _isCheckBoxEnabled;
+            set => SetProperty(ref _isCheckBoxEnabled, value);
+        }
+
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        public MainViewModel()
+        {
+            BtnCmd = new AsyncRelayCommand(DoSomething, CanDoSomething);
+            CheckBoxCmd = new RelayCommand<bool>(Checked);
+            IsCheckBoxEnabled = true;
+            Title = $"Active: {IsCheckBoxEnabled}";
+        }
+
+        private void Checked(bool checkedState)
+        {
+            IsCheckBoxEnabled = checkedState;
+            Title = $"Active: {IsCheckBoxEnabled}";
+        }
+
+        private bool CanDoSomething()
+        {
+            return IsCheckBoxEnabled;
+        }
+
+        private async Task DoSomething()
+        {
+            IsCheckBoxEnabled = false;
+            await Task.Delay(1000);
+            IsCheckBoxEnabled = true;
+        }
+    }
+}
